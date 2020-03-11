232e9d11e14377cfe7c708276e69b6a50d3c00df
Rasha Abdulrazzak <rasha.abdulrazzak@gmail.com>
2020-03-11 05:06:25 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
         <h1>
           <pre>TwitterMessage</pre>
         </h1>
-        <TwitterMessage maxChars={280} />
+        <TwitterMessage maxChars={280} handleChange={this.handleChange} />
 
 
 
