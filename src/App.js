fd1b699f1a29a512cf83102ffb94fb0913d264c8
Rasha Abdulrazzak <rasha.abdulrazzak@gmail.com>
2020-03-11 05:10:19 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import LoginForm from "./components/LoginForm";
 import TwitterMessage from "./components/TwitterMessage";
 
 class App extends Component {
-
+constructor(){
+  super()
+  this.state ={
+    maxChars : 280 ,
+    message : ''
+  }
+}
   login = ({ username, password }) => {
     console.log(`Logging in ${username} with password ${password}`);
   };
   handeChange =(event)=>{
     this.setState({
-      inputsh:event.target.value,
-      remainingItem:this.props.maxChars - this.state.inputsh.length
+      message:event.target.value,
+      message:this.state.maxChars - this.state.message.length
     })}
 
 
