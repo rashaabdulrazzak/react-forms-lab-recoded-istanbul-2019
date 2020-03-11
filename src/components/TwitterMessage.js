32ca20c0ca81aa5c85c0a193d8b2cf53551a0328
Rasha Abdulrazzak <rasha.abdulrazzak@gmail.com>
2020-03-11 05:00:39 +0000

    Automatically backed up by Learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,24 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      inputsh : "",
+      remainingItem : 280
+    };
   }
-
+handeChange =(event)=>{
+  this.setState({
+    inputsh:event.target.value,
+    remainingItem:this.props.maxChars - this.state.inputsh.length
+  })
+  
+  
+}
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" value={this.props.maxChars} onChange={this.handeChange}/>
       </div>
     );
   }
